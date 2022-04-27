507f343148fde758e6cf10318280b4d728de33ac
Tatiana Nikonova <taniushka_2007@mail.ru>
2022-04-27 11:25:50 +0300

    Разметка

--- src/components/ItemClassComponent.js
+++ src/components/ItemClassComponent.js
@@ -4,29 +4,29 @@ import ItemModel from "../models/ItemModel";
 
 class UserProfile extends React.Component {
 
-    render() {
-        const {item} = this.props;
-        return (<>
-            <div className="main-content">
-                <h2>{item.brand}</h2>
-                <h1>{item.title}</h1>
-                <h3>{item.description}</h3>
-                <div className="description">{item.descriptionFull}</div>
-                <div className="highlight-window mobile">
-                    <div className="highlight-overlay"></div>
-                </div>
-                <div className="divider"></div>
-                <div className="purchase-info">
-                    <div className="price">{item.currency}{item.price}</div>
-                    <button>Добавить в корзину</button>
-                </div>
-            </div>
-        </>);
-    }
+	render() {
+		const { item } = this.props;
+		return (<>
+			<div className="main-content">
+				<h2>{item.brand}</h2>
+				<h1>{item.title}</h1>
+				<h3>{item.description}</h3>
+				<div className="description">
+					{item.descriptionFull}
+				</div>
+				<div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+				<div className="divider"></div>
+				<div className="purchase-info">
+					<div className="price">{item.currency}{item.price.toFixed(2)}</div>
+					<button>Добавить в корзину</button>
+				</div>
+			</div>
+		</>);
+	}
 }
 
 UserProfile.propTypes = {
-    user: PropTypes.instanceOf(ItemModel).isRequired
+	user: PropTypes.instanceOf(ItemModel).isRequired
 }
 export default UserProfile;
 
